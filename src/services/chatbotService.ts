32cce58f0f8ead6d216ee915f453469de330d06f
MathCoimbra <matheuscoimbra35@gmail.com>
2025-02-15 20:07:30 -0300

    feat: implement user interaction tracking to send welcome message
    Signed-off-by: MathCoimbra <matheuscoimbra35@gmail.com>

--- src/services/chatbotService.ts
+++ src/services/chatbotService.ts
@@ -1,6 +1,7 @@
 import { Response } from 'express';
 import { WhatsAppWebhookEvent } from "../types/whatsapp";
 import { WhatsappService } from './whatsappService';
+import redisClient from '../../utils/redisClient';
 
 // serviço de envio da mensagem pelo whatsapp
 export class ChatbotService {
@@ -23,9 +24,23 @@ export class ChatbotService {
         const from = body.entry[0].changes[0].value.messages[0].from;
         const text = body.entry[0].changes[0].value.messages[0].text.body;
         const name = body.entry[0].changes[0].value.contacts[0].profile.name;
+        console.log(`Mensagem recebida de ${name} com a mensagem: ${text}`);
 
-        console.log(`Mensagem recebida de ${from} com a mensagem: ${text}`);
-        const reply = `Olá! Tudo bem e você ${name}?`;
+        const userKey = `user${from}:firstInteraction`;
+        const hasInteracted = await redisClient.get(userKey);
+        console.log("userKey:::", userKey)
+
+        // verifica se é a primeira interação do usuário
+        if (!hasInteracted) {
+          // se não inclui, então inclui como a primeira interação 
+          await redisClient.set(userKey, "true", 'EX', 86400);
+          // e manda mensagem de boas vindas
+          await WhatsappService.sendMessage(from, this.welcomeMessage());
+          res.status(200).send('Mensagem de boas-vindas enviada com sucesso!');
+          return;
+        }
+
+        const reply = this.getResponse(text, name);
 
         await WhatsappService.sendMessage(from, reply);
         res.status(200).send('Resposta enviada com sucesso!');
@@ -34,5 +49,18 @@ export class ChatbotService {
       console.error('Erro ao enviar a mensagem: ', error.response?.data || error.message);
       throw new Error('Ocorreu algum erro ao enviar a mensagem.');
     }
+  }
+
+  static getResponse(message: string, name: string): string {
+
+    if (message === "1") {
+      return "ok registrado";
+    } else return 'Não entendi sua solicitação, por favor selecione uma das opções 🙂';
+  }
+
+  static welcomeMessage(): string {
+    return "👋 Olá! Bem-vindo à Pizzaria! Nosso cardápio é o seguinte:\n\n" +
+      "1️⃣ Pizza de Calabresa\n2️⃣ Pizza Portuguesa\n3️⃣ Pizza Frango com Catupiry\n" +
+      "\nEscolha o número da pizza que deseja pedir! 🍕";
   }
 }
