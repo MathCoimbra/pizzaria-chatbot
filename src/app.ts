23d57365236012cb33be869e1b7253e5ce605cb1
MathCoimbra <matheuscoimbra35@gmail.com>
2025-11-03 19:27:26 -0300

    refactor: separate express app and server setup
    Signed-off-by: MathCoimbra <matheuscoimbra35@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,4 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+export { app };
